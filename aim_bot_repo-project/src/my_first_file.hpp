4305f050b469471cdffd1a27ea910e6c2fdb4c1c
Nathan Duncan <20ntd1@queensu.ca>
2023-11-06 14:13:40 -0500

    Renames project for QKRT aim bot

--- aim_bot_repo-project/src/my_first_file.hpp
+++ aim_bot_repo-project/src/my_first_file.hpp
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2020-2021 Advanced Robotics at the University of Washington <robomstr@uw.edu>
+ * Copyright (c) 2020-2021 QKRT
  *
- * This file is part of taproot-template-project.
+ * This file is part of aim_bot_repo.
  *
- * taproot-template-project is free software: you can redistribute it and/or modify
+ * aim_bot_repo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * taproot-template-project is distributed in the hope that it will be useful,
+ * aim_bot_repo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with taproot-template-project.  If not, see <https://www.gnu.org/licenses/>.
+ * along with aim_bot_repo.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef MY_FIRST_FILE_HPP_
