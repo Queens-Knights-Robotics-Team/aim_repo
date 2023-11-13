e4d2adb881222ac93d1d1e52632831f2b3f8dc8f
juliamdyck <20jmd7@queensu.ca>
2023-11-13 18:51:43 -0500

    Setting up Indexer Subsystem

---
+++ aimbot-project/src/Indexer/IndexerSubsystem.hpp
@@ -0,0 +1,21 @@
+
+#pragma once 
+
+#include "aimbot-project/taproot/src/tap/motor/dji_motor.hpp 
+
+
+Class Drivers;
+
+namespace control::yaw_motor
+
+/** 
+ * Subsystem whose primary purpose is to move the yaw motor in open loop control from the controller 
+ * 
+*/
+
+{
+    Class IndexerSubsystem 
+    {
+        public
+    }
+}
